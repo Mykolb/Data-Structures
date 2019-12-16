a8425317bbce4f8785deec8ffe6589d637d59a53
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-12-16 18:57:38 -0500

    one test failing in queue and one in stack

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,29 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+"""
+LIFO data structure 
+-> last element added to the stack, will be the first element removed from stack 
+-> used when you want undo an action that was at the top of the stack like history object when using React 
+"""
+#should have head, tail, length
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+       self.storage.add_to_tail(value)
+       self.size += 1
 
     def pop(self):
-        pass
+        if self.size > 0:
+            value = self.storage.remove_from_tail()
+            self.size -= 1
+            return value 
+        else:
+            return None
 
     def len(self):
-        pass
+        return self.size
